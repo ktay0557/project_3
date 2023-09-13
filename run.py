19f481d0534d548a3dc7ca4e679274cf2405c1f7
ktay0557 <>
2023-09-13 15:14:46 +0000

    Create get_miles_data function to get data string for each day and member from user

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import calendar
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +13,24 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('felling_running_club')
 
-miles = SHEET.worksheet('miles')
 
-data = miles.get_all_values()
+def get_miles_data():
+    """
+    Get the daily miles for each member for the user
+    """
+    print("Data should be five numbers, separated by commas.")
+    print("Example: 12,5,8,6,7\n")
+    week = list(calendar.day_name)
+    miles_data = {}
 
-print(data)
+    for day in week:
+        print(f"Please enter the miles data from {day}, for each member.")
+        data_str = input("Enter the data here: ")
+        miles_data[day] = data_str
+
+    print("\n")
+
+    for day, data in miles_data.items():
+        print(f"The miles provided for {day} are {data_str}")
+
+get_miles_data()
